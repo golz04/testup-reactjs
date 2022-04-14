e70180b11dbec29a54b868cb34d9e9c6a9e3525f
golz04 <muhammadhaidar862@gmail.com>
2022-04-15 01:14:31 +0700

    learn jsx with es6

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const user = "Haidar";
+  const status = <b>Lord Is Back !</b>;
+  const nilaiAwal = 1;
+  const nilaiAkhir = 2;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{user}</p>
+      <p>{status}</p>
+      <h3>{nilaiAwal + nilaiAkhir}</h3>
     </div>
   );
 }
